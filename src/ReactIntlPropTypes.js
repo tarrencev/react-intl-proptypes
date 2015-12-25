71d1e438e556966e280eccf11ad2a6751d71bff0
Tarrence van As <tarrence@box.com>
2015-12-24 22:21:43 -0700

    Use airbnb eslint config

--- src/ReactIntlPropTypes.js
+++ src/ReactIntlPropTypes.js
@@ -1,73 +1,74 @@
 // Heavily inspired by the ReactPropTypes.js implementation found in React.js
 import {
-	FormattedDate,
-	FormattedHTMLMessage,
-	FormattedMessage,
-	FormattedNumber,
-	FormattedPlural,
-	FormattedRelative,
-	FormattedTime,
+  FormattedDate,
+  FormattedHTMLMessage,
+  FormattedMessage,
+  FormattedNumber,
+  FormattedPlural,
+  FormattedRelative,
+  FormattedTime,
 } from 'react-intl';
 
 const ANONYMOUS = '<<anonymous>>';
 
-const ReactIntlPropTypes = {
-	formattedDate: createReactIntlTypeChecker(FormattedDate),
-	formattedHTMLMessage: createReactIntlTypeChecker(FormattedHTMLMessage),
-	formattedMessage: createReactIntlTypeChecker(FormattedMessage),
-	formattedNumber: createReactIntlTypeChecker(FormattedNumber),
-	formattedPlural: createReactIntlTypeChecker(FormattedPlural),
-	formattedRelative: createReactIntlTypeChecker(FormattedRelative),
-	formattedTime: createReactIntlTypeChecker(FormattedTime),
-};
-
 // Equivalent of `typeof` but with special handling for array and regexp.
 function getPropType(propValue) {
-	if (Array.isArray(propValue)) {
-		return 'array';
-	}
-	if (propValue instanceof RegExp) {
-		// Old webkits (at least until Android 4.0) return 'function' rather than
-		// 'object' for typeof a RegExp. We'll normalize this here so that /bla/
-		// passes PropTypes.object.
-		return 'object';
-	}
-	return typeof propValue;
+  if (Array.isArray(propValue)) {
+    return 'array';
+  } else if (propValue instanceof RegExp) {
+    // Old webkits (at least until Android 4.0) return 'function' rather than
+    // 'object' for typeof a RegExp. We'll normalize this here so that /bla/
+    // passes PropTypes.object.
+    return 'object';
+  }
+  return typeof propValue;
 }
 
 function createChainableTypeChecker(validate) {
-	function checkType(isRequired, props, propName, componentName = ANONYMOUS, location) {
+  function checkType(isRequired, props, propName, componentName = ANONYMOUS, location) {
+    if (props[propName] === null) {
+      if (isRequired) {
+        return new Error(
+          `Required ${location} \`${propName}\` was not specified in ` +
+          `\`${componentName}\`.`
+        );
+      }
+    } else {
+      return validate(props, propName, componentName, location);
+    }
+  }
 
-	if (props[propName] == null) {
-		if (isRequired) {
+  const chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
 
-			return new Error(
-				`Required ${location} \`${propName}\` was not specified in ` +
-				`\`${componentName}\`.`
-			);
-		}
-	} else {
-		return validate(props, propName, componentName, location);
-	}
+  return chainedCheckType;
 }
 
 function createReactIntlTypeChecker(expectedType) {
-	function validate(props, propName, componentName, location) {
-		const propValue = props[propName];
+  function validate(props, propName, componentName, location) {
+    const propValue = props[propName];
 
-		if (propValue !== instanceof expectedType) {
-			const propType = getPropType(propValue);
+    if (!(propValue instanceof expectedType)) {
+      const propType = getPropType(propValue);
 
-			return new Error(
-				`Invalid ${location} \`${propName}\` of type \`${propType}\` ` +
-				`supplied to \`${componentName}\`, expected \`${expectedType}\`.`
-			);
-		}
+      return new Error(
+        `Invalid ${location} \`${propName}\` of type \`${propType}\` ` +
+        `supplied to \`${componentName}\`, expected \`${expectedType}\`.`
+      );
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	return createChainableTypeChecker(validate);
+  return createChainableTypeChecker(validate);
 }
 
-export default ReactIntlPropTypes;
+export default {
+  formattedDate: createReactIntlTypeChecker(FormattedDate),
+  formattedHTMLMessage: createReactIntlTypeChecker(FormattedHTMLMessage),
+  formattedMessage: createReactIntlTypeChecker(FormattedMessage),
+  formattedNumber: createReactIntlTypeChecker(FormattedNumber),
+  formattedPlural: createReactIntlTypeChecker(FormattedPlural),
+  formattedRelative: createReactIntlTypeChecker(FormattedRelative),
+  formattedTime: createReactIntlTypeChecker(FormattedTime),
+};
